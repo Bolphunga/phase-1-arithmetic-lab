bf223bb99a511957955bf9d768c7156f275deb47
Bolphunga <aymendj@yahoo.com>
2023-07-23 23:33:22 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 25;
+const num4 = 7;
+let multiply;
+let random;
+let mod;
+let max;
+
+multiply = num1 * num2;
+
+random = parseInt(Math.random() + multiply);
+
+mod = num3 % num4;
+
+max = Math.max(1, 5, 7, 9, 20);
